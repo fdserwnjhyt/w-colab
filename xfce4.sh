6d0b03a636a74d0f33df17c7d051ea6bfe21babf
fdserwnjhyt <fdserwnjhyt@gmail.com>
2024-09-04 15:20:57 +0300

    Update xfce4.sh

--- xfce4.sh
+++ xfce4.sh
@@ -17,6 +17,7 @@ sudo apt install --assume-yes xscreensaver > /dev/null 2>&1
 sudo systemctl disable lightdm.service > /dev/null 2>&1
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb > /dev/null 2>&1
 sudo apt install --assume-yes --fix-broken ./google-chrome-stable_current_amd64.deb > /dev/null 2>&1
+sudo apt install firefox -y > /dev/null 2>&1
 echo "===================================="
 echo "Start RDP"
 echo "===================================="
