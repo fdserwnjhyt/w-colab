93167a4b6f32a5d7b206be257b7eadfd4c313cbe
fdserwnjhyt <fdserwnjhyt@gmail.com>
2024-09-09 09:19:59 +0300

    Update xfce4.sh

--- xfce4.sh
+++ xfce4.sh
@@ -5,18 +5,13 @@ echo "deb [arch=amd64] https://dl.google.com/linux/chrome-remote-desktop/deb sta
 sudo apt-get update
 sudo DEBIAN_FRONTEND=noninteractive \
     apt-get install --assume-yes chrome-remote-desktop
-echo "===================================="
-echo "Install RDP"
-echo "===================================="
-sudo apt install --assume-yes xfce4 desktop-base dbus-x11 xscreensaver > /dev/null 2>&1
-sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session' > /dev/null 2>&1
-sudo systemctl disable lightdm.service > /dev/null 2>&1
-sudo apt install --assume-yes xfce4 desktop-base xfce4-terminal > /dev/null 2>&1
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb > /dev/null 2>&1
-sudo apt install --assume-yes --fix-broken ./google-chrome-stable_current_amd64.deb > /dev/null 2>&1
-read -p "Paste code here (Copy and Ctrl+V to paste then press Enter): " CRP
-echo "===================================="
-echo "Start RDP"
-echo "===================================="
-echo "===================================="
+sudo DEBIAN_FRONTEND=noninteractive \
+    apt install --assume-yes xfce4 desktop-base dbus-x11 xscreensaver
+    sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'
+    sudo systemctl disable lightdm.service
+    sudo apt install --assume-yes task-xfce-desktop
+    curl -L -o google-chrome-stable_current_amd64.deb \
+https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install --assume-yes --fix-broken ./google-chrome-stable_current_amd64.deb
+sudo apt install firefox -y
 seq 1 43200 | while read i; do echo -en "\r Running .     $i s /43200 s";sleep 0.1;echo -en "\r Running ..    $i s /43200 s";sleep 0.1;echo -en "\r Running ...   $i s /43200 s";sleep 0.1;echo -en "\r Running ....  $i s /43200 s";sleep 0.1;echo -en "\r Running ..... $i s /43200 s";sleep 0.1;echo -en "\r Running     . $i s /43200 s";sleep 0.1;echo -en "\r Running  .... $i s /43200 s";sleep 0.1;echo -en "\r Running   ... $i s /43200 s";sleep 0.1;echo -en "\r Running    .. $i s /43200 s";sleep 0.1;echo -en "\r Running     . $i s /43200 s";sleep 0.1; done
