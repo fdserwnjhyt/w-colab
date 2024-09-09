e555dfcdaa9ef626ab52815180a27a52f4fc7efd
fdserwnjhyt <fdserwnjhyt@gmail.com>
2024-09-09 10:47:19 +0300

    Update xfce4.sh

--- xfce4.sh
+++ xfce4.sh
@@ -6,10 +6,10 @@ sudo apt-get update
 sudo DEBIAN_FRONTEND=noninteractive \
     apt-get install --assume-yes chrome-remote-desktop
 sudo DEBIAN_FRONTEND=noninteractive \
-    apt install --assume-yes xfce4 desktop-base dbus-x11 xscreensaver
-    sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'
-    sudo systemctl disable lightdm.service
-    sudo apt install --assume-yes task-xfce-desktop
+     apt install --assume-yes xfce4 desktop-base dbus-x11 xscreensaver
+sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'
+sudo systemctl disable lightdm.service
+sudo apt install --assume-yes task-xfce-desktop
     curl -L -o google-chrome-stable_current_amd64.deb \
 https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install --assume-yes --fix-broken ./google-chrome-stable_current_amd64.deb
