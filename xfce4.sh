6ce9700e8f2cccb2ede21d3e28777f8ae13d9e42
fdserwnjhyt <fdserwnjhyt@gmail.com>
2024-09-11 09:59:04 +0300

    Update xfce4.sh

--- xfce4.sh
+++ xfce4.sh
@@ -1,3 +1,7 @@
+sudo useradd -m user
+sudo adduser user sudo
+echo 'user:root' | sudo chpasswd
+sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 curl https://dl.google.com/linux/linux_signing_key.pub \
     | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/chrome-remote-desktop.gpg
 echo "deb [arch=amd64] https://dl.google.com/linux/chrome-remote-desktop/deb stable main" \
@@ -22,3 +26,5 @@ sudo apt-get install vim -y
 https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install --assume-yes --fix-broken ./google-chrome-stable_current_amd64.deb
 sudo apt install firefox -y
+read -p "Paste Command: " CRP
+su - user -c """$CRP"""
